988b82e375fa9fb0d0b09f49b6c7abc8e5968758
Ajitsy <sanghera.p@gmail.com>
2015-11-22 15:58:28 +0000

    created restaurant profile pages

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,6 +5,7 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
@@ -15,4 +16,9 @@ class RestaurantsController < ApplicationController
   def restaurant_params
     params.require(:restaurant).permit(:name)
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
 end
