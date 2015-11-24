dc4c4d7739a30d5373ab3eeb3e2a34a49e5b03b5
Ajitsy <sanghera.p@gmail.com>
2015-11-24 17:52:27 +0000

    user can sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+
+  root to: "restaurants#index"
+
   resources :restaurants do
     resources :reviews
-  end 
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
